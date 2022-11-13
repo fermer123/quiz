de6f864376f6cb90269e21edb7662f186191adb5
fermer123 <fermer_525@mail.ru>
2022-11-13 17:20:00 +0500

    step

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Game from './components/Game/Game';
 import Result from './components/Result/Result';
+import data from './components/data/data.json';
 import './index.scss';
 
 const App: React.FC = () => {
+  const [step, setStep] = useState<number>(0);
+  const question = data[step];
   return (
     <div className='App'>
-      <Game />
+      <Game question={question} step={step} setStep={setStep} />
       {/* <Result /> */}
     </div>
   );
--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,16 +1,34 @@
 import style from './Game.module.scss';
+type IQuestion = {
+  title: string;
+  variants: Array<string>;
+  correct: number;
+};
+interface GameProps {
+  step: number;
+  setStep: (step: number) => void;
+  question: IQuestion;
+}
 
-const Game: React.FC = () => {
+const Game: React.FC<GameProps> = ({ step, setStep, question }) => {
+  const onClick = (idx: number) => {
+    console.log('idx:', idx, 'step:', step);
+    setStep(step + 1);
+  };
+  // const percent = (step / question.lenght) * 100;
+
   return (
     <div>
       <div className={style.progress}>
-        <div style={{ width: '50%' }} className={style.inner}></div>
+        <div style={{ width: '20%' }} className={style.inner}></div>
       </div>
-      <h1 style={{ marginBottom: '25px' }}>Вопрос</h1>
+      <h1 style={{ marginBottom: '25px' }}>{question.title}</h1>
       <ul>
-        <li>выбор 1</li>
-        <li>выбор 2</li>
-        <li>выбор 3</li>
+        {question.variants.map((e, idx) => (
+          <li onClick={() => onClick(idx)} key={idx}>
+            {e}
+          </li>
+        ))}
       </ul>
     </div>
   );
